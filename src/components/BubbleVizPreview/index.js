8fb02229cf0686c4cc5323915c3ae8728b991da7
Stefan Hochwarter <stefan.hochwarter@gmail.com>
2018-06-07 16:18:59 -0400

    added bubble viz preview component

---
+++ src/components/BubbleVizPreview/index.js
@@ -0,0 +1,119 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import ephtrackingViz from "ephtracking-viz";
+import * as d3 from "d3";
+
+class BubbleVizPreview extends Component {
+  constructor() {
+    super();
+    this.state = {
+      intervalId: null
+    };
+  }
+  componentDidMount() {
+    const {
+      xMeasureId,
+      xLabel,
+      yMeasureId,
+      yLabel,
+      title,
+      temporal
+    } = this.props;
+    const options = {
+      type: 'bubble',
+      title,
+      data: {
+        x: {
+          measureId: xMeasureId,
+          label: xLabel
+        },
+        y: {
+          measureId: yMeasureId,
+          label: yLabel
+        },
+        temporal
+      }
+    };
+    console.log(options);
+    ephtrackingViz.createVisualization(this.svg, options);
+  }
+
+  shouldComponentUpdate() {
+    // this prevents future re-renders of this component
+    return false;
+  }
+
+  componentWillReceiveProps(newProps) {
+    clearTimeout(this.state.intervalId);
+    // clear intervals
+    this.clearAllIntervals();
+    d3
+      .select(this.svg)
+      .selectAll("*")
+      .remove();
+    const {
+      xMeasureId,
+      xLabel,
+      yMeasureId,
+      yLabel,
+      title,
+      temporal
+    } = this.props;
+    const options = {
+      type: 'bubble',
+      title,
+      data: {
+        x: {
+          measureId: xMeasureId,
+          label: xLabel
+        },
+        y: {
+          measureId: yMeasureId,
+          label: yLabel
+        },
+        temporal
+      }
+    };
+
+    // to avoid overdrawing
+    const intervalId = setTimeout(
+      () => ephtrackingViz.createVisualization(this.svg, options),
+      200
+    );
+    this.setState({ intervalId });
+  }
+
+  clearAllIntervals() {
+    for (let i = 1; i < 99999; i++) {
+      window.clearInterval(i);
+    }
+  }
+
+  render() {
+    let width = 700;
+    let height = 700;
+    return (
+      <svg
+        width={width}
+        height={height}
+        ref={elem => {
+          this.svg = elem;
+        }}
+      />
+    );
+  }
+}
+
+BubbleVizPreview.propTypes = {
+  title: PropTypes.string,
+  temporal: PropTypes.oneOfType([
+    PropTypes.string.isRequired,
+    PropTypes.arrayOf(PropTypes.string)
+  ]),
+  xMeasureId: PropTypes.string.isRequired,
+  xLabel: PropTypes.string.isRequired,
+  yMeasureId: PropTypes.string.isRequired,
+  yLabel: PropTypes.string.isRequired
+};
+
+export default BubbleVizPreview;
--- src/pages/BubbleChart/index.js
+++ src/pages/BubbleChart/index.js
@@ -2,32 +2,79 @@ import React, { Component } from 'react';
 import Title from '../../components/Title';
 import CIM from '../../components/CIM';
 import SelectYearsFromTwoMeasures from '../../containers/SelectYearsFromTwoMeasures';
+import ErrorBoundary from '../../components/ErrorBoundary';
+import BubbleVizPreview from '../../components/BubbleVizPreview';
+import Code from '../../components/Code';
 
 class BubbleChart extends Component {
   state = {
-    title: '',
+    title: null,
     xMeasureId: null,
+    xLabel: null,
     yMeasureId: null,
-    years: []
+    yLabel: null,
+    years: null,
+    view: 'preview'
   };
 
   handleTitleChange = title => {
     this.setState({ title });
   }
 
-  handleXMeasureIdChange = xMeasureId => {
-    this.setState({ xMeasureId });
+  handleXMeasureIdChange = (xMeasureId, xLabel) => {
+    this.setState({ xMeasureId, xLabel });
   }
 
-  handleYMeasureIdChange = yMeasureId => {
-    this.setState({ yMeasureId });
+  handleYMeasureIdChange = (yMeasureId, yLabel) => {
+    this.setState({ yMeasureId, yLabel });
   }
 
   handleYearsChange = years => {
     this.setState({ years });
   }
 
+  setView = view => this.setState({ view });
+
   render() {
+    const {
+      title,
+      xMeasureId,
+      xLabel,
+      yMeasureId,
+      yLabel,
+      years,
+      view
+    } = this.state;
+    const isValid = title && xMeasureId && yMeasureId && years && years.length > 0;
+    let temporal;
+    if (years && years.length > 0) {
+      const min = Number.parseInt(years[0], 10);
+      const max = Number.parseInt(years[years.length - 1], 10);
+      if (min === max) {
+        temporal = min;
+      } else if (max === min + years.length - 1){
+        temporal = `${min}-${max}`;
+      } else {
+        temporal = years;
+      }
+    }
+
+    const options = `var options = {
+      type: 'bubble',
+      title: '${title}',
+      data: {
+        x: {
+          measureId: '${xMeasureId}',
+          xLabel: '${xLabel}'
+        },
+        y: {
+          measureId: '${yMeasureId}',
+          yLabel: '${yLabel}'
+        },
+        temporal: '${temporal}'
+      };`
+
+
     return (
       <div className="container">
         <h1 className="title">Bubble chart</h1>
@@ -45,6 +92,40 @@ class BubbleChart extends Component {
           secondMeasureId={this.state.yMeasureId}
           onChange={this.handleYearsChange}
         />
+
+        <div className="tabs">
+        <ul>
+          <li onClick={() => this.setView('preview')} className={view === 'preview' ? 'is-active' : ''}>
+            <a>
+              <span className="icon is-small"><i className="fa fa-globe"></i></span>
+              <span>Preview</span>
+            </a>
+          </li>
+          <li onClick={() => this.setView('code')} className={view === 'code' ? 'is-active': ''}>
+            <a>
+              <span className="icon is-small"><i className="fa fa-code"></i></span>
+              <span>Code</span>
+            </a>
+          </li>
+        </ul>
+      </div>
+
+      { view === 'preview' && isValid &&
+        <ErrorBoundary>
+          <BubbleVizPreview
+            type="bubble"
+            title={title}
+            xMeasureId={xMeasureId}
+            xLabel={xLabel}
+            yMeasureId={yMeasureId}
+            yLabel={yLabel}
+            temporal={temporal}
+          />
+        </ErrorBoundary>
+      }
+
+      { view === 'code' && <Code options={options} width={700} height={700} />}
+
       </div>
     );
   }
